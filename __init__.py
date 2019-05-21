47a62c5b447a03984ee98a4916511fd647bcdcde
colla69 <colla69@gmail.com>
2019-05-21 17:46:46 +0200

    it seems to be working :)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from collections import defaultdict
 
@@ -20,32 +21,55 @@ def refresh_library(path):
 
 
 class PlexMusicSkill(CommonPlaySkill):
+
     def CPS_match_query_phrase(self, phrase):
-        LOG.info("local music")
         title, t_prob = self.title_search(phrase)
         artist, a_prob = self.artist_search(phrase)
         album, al_prob = self.album_search(phrase)
         print("""
-%s %f
-%s %d
-%s %d        
+        %s %f
+        %s %d
+        %s %d        
         """ % (title, t_prob, artist, a_prob, album, al_prob))
-        data = {
-            "title": title,
-            "file": self.titles[title]
-        }
-        return (phrase, CPSMatchLevel.EXACT, data)
+
+        if t_prob > al_prob and t_prob > a_prob:
+            data = {
+                "title": title,
+                "file": self.titles[title]
+            }
+            return phrase, CPSMatchLevel.TITLE, data
+        elif a_prob >= al_prob:
+            data = {
+                "title": artist,
+                "file": self.artists[artist]
+            }
+            return phrase, CPSMatchLevel.MULTI_KEY, data
+        elif al_prob > a_prob:
+            data = {
+                "title": album,
+                "file": self.albums[album]
+            }
+            return phrase, CPSMatchLevel.MULTI_KEY, data
+        else:
+            return None
 
     def CPS_start(self, phrase, data):
-        # print("playing")
-        # print(data)
-        # os.system('cvlc "%s" ' % data["file"])
-        # os.system('vlc --intf dummy "%s" ' % data["file"])
-        # print(data["file"][0])
-        m = self.vlcI.media_new(data["file"][0])
-        self.player.set_media(m)
-        # player.add_.MediaPlayer('http://plex.colarietitosti.info:32400/library/parts/12226/1557727332/file.mp3?download=1&X-Plex-Token=y9pLd6uPWXpwbw14sRYf')
-        self.player.play()
+        if self.get_running():
+            self.player.stop()
+        title = data["title"]
+        link = data["file"]
+        try:
+            m = self.vlcI.media_list_new(link)
+            self.player.set_media_list(m)
+            self.player.play()
+        except Exception as e:
+            print(type(e))
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        finally:
+            time.sleep(2)
+            if not self.get_running():
+                self.speak("playback problem")
 
     def __init__(self):
         super().__init__(name="TemplateSkill")
@@ -54,11 +78,15 @@ class PlexMusicSkill(CommonPlaySkill):
         self.albums = defaultdict(list)
         self.titles = defaultdict(list)
         self.vlcI = vlc.Instance()
-        self.player = self.vlcI.media_player_new()
+        self.player = self.vlcI.media_list_player_new()
+        self.get_play_status()
 
+    def get_play_status(self):
+        self.play_status = self.player.is_playing()
 
-    def initialize(self):
-        self.load_data()
+    def get_running(self):
+        self.get_play_status()
+        return self.play_status
 
     def load_data(self):
         datapath = os.path.expanduser(self.music_source.strip() + "data.json")
@@ -83,6 +111,9 @@ class PlexMusicSkill(CommonPlaySkill):
                     self.albums[album].append(file)
                     self.artists[artist].append(file)
                     self.titles[title].append(file)
+
+    def initialize(self):
+        self.load_data()
 
     def json_save(self, data, fname):
         with open(fname, 'w') as fp:
@@ -111,27 +142,28 @@ class PlexMusicSkill(CommonPlaySkill):
         return album, confidence
 
     @intent_file_handler('play.music.intent')
-    def handle_play_music_ntent(self, message):
-        self.activate_player()
-
+    def handle_play_music_intent(self, message):
+        pass
 
     @intent_file_handler('pause.music.intent')
     def handle_pause_music_intent(self, message):
-        self.activate_player()
+        self.player.pause()
 
+    @intent_file_handler('next.music.intent')
+    def handle_next_music_intent(self, message):
+        self.player.next()
 
     @intent_file_handler('reload.library.intent')
     def handle_reload_library_intent(self, message):
-        refresh_library(self.music_source)
         self.speak_dialog("refresh.library")
+        self.load_data()
 
     def converse(self, utterances, lang="en-us"):
         return False
 
     def stop(self):
-        pass
-        # if getrunning():
-        #  self.stop_player()
+        self.player.stop()
+        self.vlcI.release()
 
 
 def create_skill():
